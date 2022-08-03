db376441d1d436042f00ba6ebacae4bcbb7067ca
Kevin McLaughlin <92177377+coding-kevin@users.noreply.github.com>
2022-08-03 11:17:43 -0700

    Update main.js
    Cleaned up comments

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 document.getElementById("submit").addEventListener("click", function () {
+  
+  //Get user input
   var userInput = document.getElementById("text").value;
-  //blank error
   if (userInput == "") {
     alert("Invalid Entry.");
   }
 
+  // Count letters with RegEx
   var characters = (userInput.match(/[a-zA-Z]/g) || []).length;
   var sentences = (userInput.match(/\?|\!|\./g) || []).length;
   var words = (userInput.match(/\s/g) || []).length + 1;
@@ -14,15 +16,18 @@ document.getElementById("submit").addEventListener("click", function () {
     0.0588 * avgCharacterCount - 0.296 * avgSentenceCount - 15.8
   );
 
+  //ensure length of input is sufficient
   if (userInput.length < 10 || sentences < 2) {
     alert("Please enter at least four sentences of text");
   }
 
+  //handle grade levels that are below 1st grade
   if (output < 1) {
     output = "<1";
   }
-
+  
+  //output 
   document.getElementsByName("output")[0].value = output;
 });
 
-//https://stackoverflow.com/questions/46987505/how-to-count-letters-in-javascript
+
