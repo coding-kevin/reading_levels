15f83665d295cc71e12e80b2d3933f14b526953a
Kevin McLaughlin <92177377+coding-kevin@users.noreply.github.com>
2022-08-03 11:18:02 -0700

    Update main.js
    cleaned comments

--- main.js
+++ main.js
@@ -16,17 +16,17 @@ document.getElementById("submit").addEventListener("click", function () {
     0.0588 * avgCharacterCount - 0.296 * avgSentenceCount - 15.8
   );
 
-  //ensure length of input is sufficient
+  //Ensure length of input is sufficient
   if (userInput.length < 10 || sentences < 2) {
     alert("Please enter at least four sentences of text");
   }
 
-  //handle grade levels that are below 1st grade
+  //Handle grade levels that are below 1st grade
   if (output < 1) {
     output = "<1";
   }
   
-  //output 
+  //Output 
   document.getElementsByName("output")[0].value = output;
 });
 
